f745b4ca29c03154075c506994516519a9ad1853
Daffa <daffamfzn@gmail.com>
2023-11-29 19:58:01 +0700

    refactor: removed comments

--- invite/user_profile/views.py
+++ invite/user_profile/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from find_teams.models import Lamaran
 
-# Create your views here.
 def delete_application(request, application_id):
     lamaran = Lamaran.objects.get(id=application_id)
     lamaran.delete()
