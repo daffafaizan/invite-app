ef14ee49eb25597f3b6771d0af9d502abe5a22da
Daffa <daffamfzn@gmail.com>
2023-11-29 19:33:37 +0700

    feat: added ability to delete application

--- invite/find_teams/views.py
+++ invite/find_teams/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from authentication.models import RegisteredUser
 from find_teams.forms import LamaranForm
 from find_members.models import LowonganRegu
+from find_teams.models import Lamaran
 
 def show_vacancies(request):
     # TODO
@@ -32,3 +33,9 @@ def apply_vacancy(request, lowongan_id):
         form = LamaranForm()
 
     return render(request, "vacancy.html", {'form': form})
+
+def delete_application(request, application_id):
+    lamaran = Lamaran.objects.get(id=application_id)
+    lamaran.delete()
+
+    return render(request, "vacancies.html")
