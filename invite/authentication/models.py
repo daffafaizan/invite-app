bb01977c7e069bc7e6abaaf0f205acf7daccf445
Jeremy Alva Prathama <jeremy.alva@ui.ac.id>
2023-11-30 20:20:02 +0700

    feat: successfully instantiated ProfileDetails and TautanSosmed by RegisteredUser instantiation

--- invite/authentication/models.py
+++ invite/authentication/models.py
@@ -28,19 +28,31 @@ class RegisteredUser(AbstractUser):
         # File will be uploaded to MEDIA_ROOT/user_<id>/<filename>
         return f'user_{self.user.username}/{filename}'
 
+    def save(self, *args, **kwargs):
+        # Check if profile_details is not set, then create a default ProfileDetails instance
+        if not self.profile_details:
+            default_profile_details = ProfileDetails.objects.create()
+            self.profile_details = default_profile_details
+        if not self.tautan_media_sosial:
+            default_tms = TautanMediaSosial.objects.create()
+            self.tautan_media_sosial = default_tms
+        super().save(*args, **kwargs)
+
     def __str__(self) -> str:
         return self.username
 
-    middle_name = models.CharField(max_length=30, blank=True, null=True)
+    middle_name = models.CharField(max_length=30, blank=True)
 
-    universitas = models.CharField(max_length=100, blank=True, null=True) 
-    jurusan = models.CharField(max_length=100, blank=True, null=True)
-    keahlian = ArrayField(models.CharField(max_length=30, blank=True, null=True), blank=True, null=True)
+    universitas = models.CharField(max_length=100, blank=True) 
+    jurusan = models.CharField(max_length=100, blank=True)
+    keahlian = ArrayField(models.CharField(max_length=30, blank=True), blank=True, null=True)
     
     foto_profil = models.ImageField(upload_to=user_directory_path) # FOR LATER USAGE see https://stackoverflow.com/questions/64592126/how-get-image-from-images-model-to-home-page-in-django
+    tautan_portfolio = models.CharField(max_length=250, blank=True)
+
     tautan_media_sosial = models.OneToOneField(TautanMediaSosial, on_delete=models.SET_NULL, blank=True, null=True)
+
     profile_details = models.OneToOneField(ProfileDetails, on_delete=models.SET_NULL, blank=True, null=True)
-    tautan_portfolio = models.CharField(max_length=250, blank=True, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- invite/authentication/views.py
+++ invite/authentication/views.py
@@ -1,4 +1,5 @@
 import datetime, logging
+from django.forms.models import BaseModelForm
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import request, JsonResponse, HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -10,6 +11,7 @@ from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic.edit import CreateView, FormView
 from .forms import RegisteredUserCreationForm, RegisteredUserLoginForm
+from .models import TautanMediaSosial, ProfileDetails, RegisteredUser
 
 logger = logging.getLogger('app_api') # from settings.py
 
@@ -18,6 +20,43 @@ class RegisterView(CreateView):
     form_class = RegisteredUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "authentication/register.html"
+    
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        return super().form_valid(form)
+
+    def post(self, request):
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            registered_user = form.save(commit=False)
+
+            # pd = ProfileDetails.objects.create()
+            # tms = TautanMediaSosial.objects.create()
+            
+            # form.instance.profile_details = pd
+            # form.instance.tautan_media_sosial = tms
+            
+            # # Save all 3 objects
+            # logger.info("pd", pd)
+            # logger.info("tms", tms)
+
+            # pd.save()
+            # tms.save()
+
+            registered_user.save()
+            logger.info("REGISTERED user", form.cleaned_data["username"][0])
+
+            return redirect(self.success_url)
+        else:
+            message = "Login failed!"
+            context = {
+                "form": form,
+                "status_code": 500,
+                "message": message,
+            }
+
+            logger.error("FAILED, Form invalid")
+            logger.error(form.errors)
+            return render(request, self.template_name, context)
 
 class LoginView(FormView):
     form_class = RegisteredUserLoginForm #
@@ -43,7 +82,7 @@ class LoginView(FormView):
                 password=form.cleaned_data["password"]
             )
             
-            logger.info("USER", user)
+            logger.info("LOGGED IN AS", user.get_username())
 
             if user is not None:
                 login(request, user)
@@ -62,6 +101,7 @@ class LoginView(FormView):
 class LogoutView(View):
     def get(self, request):
         logout(request)
+        logger.info("LOGGED OUT of %d"%request.user.get_username())
         return redirect(reverse("core:home"))
     
 
