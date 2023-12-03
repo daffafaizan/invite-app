85af0c40694c2cebff9ecd337c3c76032d608381
Daffa <daffamfzn@gmail.com>
2023-12-03 12:22:04 +0700

    feat: modified views.py and made application a two step process

--- invite/find_teams/views.py
+++ invite/find_teams/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from authentication.models import RegisteredUser
 from find_teams.forms import LamaranForm
@@ -17,23 +17,55 @@ def show_vacancy_details(request, lowongan_id):
     # TODO
     return render(request, "vacancy.html")
 
-def apply_vacancy(request, lowongan_id):
-    current_user = RegisteredUser.objects.get(username=request.COOKIES.get("username"))
+def apply_vacancy_first(request):
+    if request.method == "POST":
+        keahlian = request.POST.get("keahlian")
+        cover_letter = request.POST.get("cover_letter")
+        portofolio = request.POST.get("portofolio")
+
+        request.session['first_page_data'] = {
+            "keahlian": keahlian,
+            "cover_letter": cover_letter,
+            "portofolio": portofolio
+        }
+
+        return redirect('find_teams:apply_vacancy_second')
+
+    return render(request, "apply_vacancy.html")
+
+def apply_vacancy_second(request, lowongan_id):
+    first_page_data = request.session.get("first_page_data", None)
+
+    if first_page_data is None:
+        return redirect("/vacancy/apply/first/")
+    
+    current_user = RegisteredUser.objects.get(id=request.COOKIES.get("user_id"))
     vacancy = LowonganRegu.objects.get(id=lowongan_id)
+    keahlian = first_page_data.get("keahlian")
+    cover_letter = first_page_data.get("cover_letter")
+    portofolio = first_page_data.get("portofolio")
 
     if request.method == "POST":
-        form = LamaranForm(request.POST)
-        if form.is_valid():
-            lamaran = form.save(commit=False)
-            lamaran.status = "Pending"
-            lamaran.pengirim = RegisteredUser.objects.get(user=current_user)
-            lamaran.penerima = vacancy.ketua
-            lamaran.lowongan = vacancy
-            lamaran.save()
+        form_data = {
+            "keahlian": keahlian,
+            "cover_letter": cover_letter,
+            "portofolio": portofolio,
+            "status": "Pending",
+            "nama": request.POST.get("nama"),
+            "universitas": request.POST.get("universitas"),
+            "jurusan": request.POST.get("jurusan"),
+            "pengirim": current_user.username,
+            "penerima": vacancy.ketua,
+            "lowongan": vacancy
+        }
+        form = LamaranForm(data=form_data)
 
-            return render(request, "application_success.html", {'lamaran': lamaran})
+        if form.is_valid():
+            form.save()
+
+            return render(request, "application_success.html", {'lamaran': form})
         
     else:
         form = LamaranForm()
 
-    return render(request, "vacancy.html", {'form': form})
+    return render(request, "apply_vacancy_second.html", {'form': form})
