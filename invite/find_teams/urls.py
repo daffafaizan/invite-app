d1fb62f0b1014b35c61ff565f73d1a57a273e461
Daffa <daffamfzn@gmail.com>
2023-12-03 12:23:04 +0700

    feat: changed urls.py based on the changes on views.py

--- invite/find_teams/urls.py
+++ invite/find_teams/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from find_teams.views import show_vacancies, show_vacancy_details, apply_vacancy
+from find_teams.views import show_vacancies, show_vacancy_details, apply_vacancy_first, apply_vacancy_second
 
 app_name = "find_teams"
 
 urlpatterns = [
     path('', show_vacancies, name='show_vacancies'),
     path('<int:lowongan_id>/details', show_vacancy_details, name='show_vacancy_details'),
-    path('<int:lowongan_id>/apply', apply_vacancy, name='apply_vacancy'),
+    path('<int:lowongan_id>/apply/first', apply_vacancy_first, name='apply_vacancy_first'),
+    path('<int:lowongan_id>/apply/second', apply_vacancy_second, name='apply_vacancy_second'),
 ]
