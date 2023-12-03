df5fe63f3160e7ec7fb762b19a361b8190cbdc21
amrul-hzz <amanda.nurul11@ui.ac.id>
2023-12-03 19:07:45 +0700

    fix: new vacancy not being saved

--- invite/find_members/views.py
+++ invite/find_members/views.py
@@ -52,33 +52,14 @@ def create_vacancy(request):
 
     form = LowonganForm()
 
-    print('!!!!!!!!!!!!!!!!! sampe sini')
-
-
     if request.method == 'POST':
         form = LowonganForm(request.POST)
-
-        print('!!!!!!!!!!!!!!!!! sampe sini0')
         
         if form.is_valid():
-            print('!!!!!!!!!!!!!!!!! sampe sini1')
-
             lowongan = form.save(commit=False)
 
             lowongan.ketua = request.user
             lowongan.is_active = True
-
-            lowongan.nama_regu = request.POST.get('nama_regu')
-            lowongan.deskripsi_lowongan_regu = request.POST.get('deskripsi_lowongan_regu')
-            lowongan.foto_lowongan_regu = request.POST.get('foto_lowongan_regu')
-
-            lowongan.nama_lomba = request.POST.get('nama_lomba')
-            lowongan.bidang_lomba = request.POST.get('bidang_lomba')
-            lowongan.tanggal_lomba = request.POST.get('tanggal_lomba')
-            lowongan.expiry = request.POST.get('expiry')
-
-            lowongan.jumlah_anggota_sekarang = request.POST.get('jumlah_anggota_sekarang')
-            lowongan.total_anggota_dibutuhkan = request.POST.get('total_anggota_dibutuhkan')
             
             tautan_medsos_regu = TautanMediaSosialLowongan(
                 website = request.POST.get('website'),
@@ -90,15 +71,11 @@ def create_vacancy(request):
             tautan_medsos_regu.save()
             lowongan.tautan_medsos_regu = tautan_medsos_regu
 
-            print('!!!!!!!!!!!!!!!!! sampe sini2')
-
             lowongan.save()
-
-            print('!!!!!!!!!!!!!!!!! sampe sini3')
 
             # if succesful, redirect to melihat daftar lowongan
             messages.success(request, 'Form submitted successfully!')
-            return redirect('find_members:show_vacancies')
+            return redirect('find_teams:show_vacancies')
         
     context = {
         'form': form
