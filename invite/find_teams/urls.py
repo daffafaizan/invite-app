38258d5eb501502e0325ea187db17153ef071b8f
Daffa <daffamfzn@gmail.com>
2023-12-04 07:22:08 +0700

    refactor: changed import method in urls

--- invite/find_teams/urls.py
+++ invite/find_teams/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from find_teams.views import show_vacancies, show_vacancy_details, apply_vacancy_first, apply_vacancy_second
+from . import views
 
 app_name = "find_teams"
 
 urlpatterns = [
-    path('', show_vacancies, name='show_vacancies'),
-    path('<uuid:lowongan_id>/details', show_vacancy_details, name='show_vacancy_details'),
-    path('<uuid:lowongan_id>/apply/first', apply_vacancy_first, name='apply_vacancy_first'),
-    path('<uuid:lowongan_id>/apply/second', apply_vacancy_second, name='apply_vacancy_second'),
+    path('', views.show_vacancies, name='show_vacancies'),
+    path('<uuid:lowongan_id>/details', views.show_vacancy_details, name='show_vacancy_details'),
+    path('<uuid:lowongan_id>/apply/first', views.apply_vacancy_first, name='apply_vacancy_first'),
+    path('<uuid:lowongan_id>/apply/second', views.apply_vacancy_second, name='apply_vacancy_second'),
 ]
