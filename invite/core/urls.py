25b2df2639dfe9c6abf3f6aa50c6fd744340fd77
Jeremy Alva Prathama <jeremy.alva@ui.ac.id>
2023-12-09 19:22:27 +0700

    feat: route home to vacancy list

--- invite/core/urls.py
+++ invite/core/urls.py
@@ -5,6 +5,6 @@ from find_teams import views as find_teams_views
 app_name = "core"
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    # path("", find_teams_views.show_vacancies, name="home"),
+    # path("", views.home, name="home"),
+    path("", find_teams_views.show_vacancies, name="home"),
 ]
