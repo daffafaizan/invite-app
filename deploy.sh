aa285756f9a0ade77196bcae46eccb16b68e4d33
Jeremy Alva Prathama <jeremy.alva@ui.ac.id>
2023-12-11 18:08:26 +0700

    refactor bash

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-# yes | y | script
-yes | docker-compose run --rm invite sh -c "python3 manage.py collectstatic"
+docker-compose run --rm invite sh -c "python3 manage.py collectstatic"
 docker-compose -f docker-compose-deploy.yml run --rm gcloud sh -c "gcloud app deploy --project rpl-a12"
 docker-compose down
