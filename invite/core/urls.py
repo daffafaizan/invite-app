51494f7b976978161376ba0b18c19aa4c0eb0b22
Jeremy Alva Prathama <jeremy.alva@ui.ac.id>
2023-12-11 13:09:20 +0700

    feat: improved searchbar style and linked update lowongan

--- invite/core/urls.py
+++ invite/core/urls.py
@@ -5,6 +5,5 @@ from find_teams import views as find_teams_views
 app_name = "core"
 
 urlpatterns = [
-    # path("", views.home, name="home"),
     path("", find_teams_views.show_vacancies, name="home"),
 ]
