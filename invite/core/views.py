b2f959466e7d7e0643dc0562bb7d40332c8bffbf
Jeremy Alva Prathama <jeremy.alva@ui.ac.id>
2023-12-11 20:07:23 +0700

    feat: bugfixes for uat

--- invite/core/views.py
+++ invite/core/views.py
@@ -14,4 +14,18 @@ def home(request):
         return redirect(reverse("authentication:login"))
     
 def error_404(request):
-    return render(request, '404.html')
+    status_code = 500
+    message = "Internal Server Error"
+    
+    if request.status_code == 404:
+        status_code = 404
+        message = "Not Found"
+    elif request.status_code == 403:
+        status_code = 403
+        message = "Forbidden"
+    
+    context = {
+        "status_code": status_code,
+        "message": message,
+    }
+    return render(request, context, '404.html')
